f73c6ef25f3ad9ff76fa432be0350e01cc12d5b3
ameda-kevin <amedakevin@gmail.com>
2024-11-01 07:05:07 +0300

    testing grapqhl and changing the path and querying via postman

---
+++ src/test/java/com/ameda/kevin/kisevu/works/BookControllerTest.java
@@ -0,0 +1,23 @@
+package com.ameda.kevin.kisevu.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+
+@GraphQlTest(BookController.class)
+class BookControllerTest {
+
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @Test
+    void getBooks(){
+        graphQlTester.documentName("books")
+                .execute()
+                .path("books")
+                .entityList(Book.class)
+                .hasSize(4);
+    }
+}
