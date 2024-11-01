3b8695bf76523906f35a393163b62649373bd3a6
ameda-kevin <amedakevin@gmail.com>
2024-11-01 05:29:55 +0300

    graphql bootstrapped

---
+++ src/test/java/com/ameda/kevin/kisevu/works/GraphQLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameda.kevin.kisevu.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
